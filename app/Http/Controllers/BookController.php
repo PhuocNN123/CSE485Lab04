046da80c2261c56983cda08a1efe7c4b1c83dedf
nguyen Ngoc phuocn <phuoc226368@gmail.com>
2024-12-21 22:57:18 +0700

    Chien Phong tu clone

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::paginate(5); 
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'category' => 'required|string|max:100',
+            'year' => 'required|integer|min:1900|max:'.date('Y'),
+            'quantity' => 'required|integer|min:1',
+        ]);
+        Book::create($request->all()); 
+        // Điều hướng sau khi lưu
+        return redirect()->route('books.index')->with('success', 'Sách đã được thêm thành công!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory;
+    public function borrows() { 
+         return $this->hasMany(Borrow::class); 
+     }
+     protected $fillable = ['name', 'author', 'category', 'year', 'quantity'];
+}

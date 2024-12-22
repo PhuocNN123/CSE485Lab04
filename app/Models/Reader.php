5303d380a3c72200eb1229910121cca2f47b2931
chienday <144938986+chienday@users.noreply.github.com>
2024-12-22 22:18:26 +0700

    Update Reader.php

--- app/Models/Reader.php
+++ app/Models/Reader.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reader extends Model
 {
-    public function borrows() { 
-        return $this->hasMany(Borrow::class); 
-    }
+   
     protected $fillable = ['name', 'birthday', 'address', 'phone']; 
 }

2b2d84f6373ae4aae94246446b5a940589778f7e
chienday <144938986+chienday@users.noreply.github.com>
2024-12-22 22:09:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\ReaderController;
 Route::get('/', function () {
     return view('readers.App');
 });
-Route::resource('books', BookController::class);
+
 Route::resource('readers', ReaderController::class);
-Route::resource('borrows', BorrowController::class);
+
